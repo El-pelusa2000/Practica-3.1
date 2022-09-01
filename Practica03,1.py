bf4a818dbffd02177c2beba3fc1a71298b2bc5d0
El-pelusa2000 <mpuga04@alumnos.uaq.mx>
2022-09-01 13:57:37 -0500

    initial commit

--- Practica03,1.py
+++ Practica03,1.py
@@ -4,19 +4,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 driver = webdriver.Chrome("./chromedriver.exe")
+driver.maximize_window()
 driver.get("https://www.clima.com")
 driver.find_element(By.XPATH, "//a[contains(@href, \'https://www.clima.com/mexico\')]").click()
 time.sleep(2)
 #con esto pondremos el estado al que queremos buscar
 driver.find_element(By.XPATH, "//input[@id=\'term\']").send_keys("Jalisco")
 time.sleep(2)
-driver.find_element_by_xpath("//span[contains(text(),'Lagos de Moreno, Estado de Jalisco')]").click()
+driver.find_element(By.CSS_SELECTOR,"li:nth-child(1) .item-name").click()
 time.sleep(2)
 driver.find_element(By.XPATH, "//a[contains(text(),\'Por horas\')]").click()
-time.sleep(2)
-driver.find_element(By.XPATH, "//a[contains(text(),\'Días\')]").click()
-time.sleep(2)
-driver.find_element(By.XPATH, "//a[contains(text(),\'Fin de semana\')]").click()
-time.sleep(2)
-driver.find_element(By.XPATH, "//a[contains(text(),\'Más info\')]").click()
-print ("Done")
